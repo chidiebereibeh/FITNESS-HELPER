121275aba306945e8532cfb3613707da69b4da79
Clara <nneamakaclara13@gmail.com>
2021-05-12 21:58:19 +0200

    Update ShoulderActivity.java

--- app/src/main/java/net/ingramintegrations/FitnessHelper/ShoulderActivity.java
+++ app/src/main/java/net/ingramintegrations/FitnessHelper/ShoulderActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ShoulderActivity extends AppCompatActivity {
 
@@ -18,6 +20,12 @@ public class ShoulderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoulder);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Shoulder!");
 
         Button previous = findViewById(R.id.previousButton);
         previous.setOnClickListener(v -> {
